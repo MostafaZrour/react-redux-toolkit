b919ecfbdca041c62f165140fffdee48bd79e5e0
Zrour Mostfa <moustazro123@gmail.com>
2024-12-21 21:06:47 +0100

    Apply Logger Middleware

--- src/redux-toolkit/store.jsx
+++ src/redux-toolkit/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { UserSlice } from "./UserSlice";
+import { logger } from "redux-logger";
 const store = configureStore({
   reducer: {
     user: UserSlice.reducer,
   },
+  middleware  :(getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 export default  store ;
